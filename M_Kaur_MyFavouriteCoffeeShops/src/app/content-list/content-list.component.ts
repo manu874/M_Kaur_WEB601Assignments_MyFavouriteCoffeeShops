dc78d61cd41fbc023c593b2b65c7f9d8765c20fe
Akshit-112 <123979642+Akshit-112@users.noreply.github.com>
2024-02-09 11:43:58 -0500

    Assignment 3 completed

--- M_Kaur_MyFavouriteCoffeeShops/src/app/content-list/content-list.component.ts
+++ M_Kaur_MyFavouriteCoffeeShops/src/app/content-list/content-list.component.ts
@@ -1,20 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
+import { ContentCardComponent } from '../content-card/content-card.component';
+import { TypedecisionPipe } from '../typedecision.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-content-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ContentCardComponent, TypedecisionPipe, FormsModule],
   templateUrl: './content-list.component.html',
   styleUrl: './content-list.component.scss'
 })
 export class ContentListComponent implements OnInit {
-  displayContentInfo(contentItem: Content) {
+  DisplayContentInformation(contentItem: Content) {
     console.log(`ID: ${contentItem.id} and Title: ${contentItem.title}`);
     }
   @Input () contentItems: Content[] = [];
 
+  searchTitle: string = '';
+  contentExists: boolean = false;
+  message: string = '';  
+  selectedTitle: string | null = null;
+
+  checkContentExists() {
+    const foundItem = this.contentItems.find(item => item.title.toLowerCase() === this.searchTitle.toLowerCase());
+    this.contentExists = !!foundItem;
+    this.message = foundItem ? 'Content item exists.' : 'Content item does not exist.';
+    this.selectedTitle = foundItem ? foundItem.title : null;
+  }
   ngOnInit(): void {
     this.contentItems =[
       {
@@ -23,14 +37,14 @@ export class ContentListComponent implements OnInit {
         description:"Tim Hortons Inc., commonly nicknamed Tim's or Timmie's, is a Canadian multinational coffeehouse and restaurant chain.",
         creator:"Jim Charade",
         imgURL:"https://www.kimp.io/wp-content/uploads/2023/08/Tim-Hortons-brand.jpg",
-        type:"Multinational",
+        type:"Famous",
         tags: ["traditional", "Tims"]
       },
       {
         id: 1,
       title: "Star Bucks",
       description:"Handcrafted Beverages: Fresh-brewed coffee, hot and iced espresso beverages, Frappuccino® coffee and non-coffee blended beverages, Starbucks Refreshers, smoothies and Tazo® teas.",
-      creator:"Jerry Baldwin",
+      creator:"Famous",
       imgURL:"https://i.abcnewsfe.com/a/8e5467c5-7156-436b-af4e-7cce95634fe0/starbucks-logo-ss-jt-240103_1704310089160_hpMain_16x9.jpg?w=992",
       type:"International Brand",
         tags: ["Starbucks", "Coffee"]
@@ -41,7 +55,7 @@ export class ContentListComponent implements OnInit {
       description:"A brand which sells coffees, burgers and fries.",
       creator:"Richard MacDonald",
       imgURL:"https://blog.tipranks.com/wp-content/uploads/2023/11/shutterstock_2323787401-750x406.jpg",
-      type:"Internations food and coffee brand",
+      type:"Famous",
         tags: ["Coffee", "Burger"]
       },
       {
@@ -50,7 +64,7 @@ export class ContentListComponent implements OnInit {
         description: "Roasters are the people who transform imported green coffee beans into beans that can then be ground, brewed and consumed by customers — a vital part of the coffee supply chain..",
         creator: "Germen Persia",
         imgURL: "https://beannorth.com/cdn/shop/files/Darkroastcoffeesubscription.png?v=1700761789",
-        type: "Roasters",
+        type: "",
         tags: ["Beans", "Coffee"]
       },
       {
@@ -59,8 +73,25 @@ export class ContentListComponent implements OnInit {
         description: "Sweet Maria's was founded by Thompson Owen in 1997 with Maria Troy, who he met in the Masters program at School of the Art Institute Chicago. Thompson (or Thom or Tom) is in charge of the green coffee side our company and does much of the writing and photography etc.",
         creator: "Thompson Owen",
         imgURL: "https://pbs.twimg.com/profile_images/714600698743242752/cmK1odH9_400x400.jpg",
-        type: "Coffee shop which is not famous",
+        type: "Local",
         tags: ["Sweet", "Underrated"]
+      },
+      {
+        id: 5,
+        title: "Mos Mos coffee",
+        description: "Mos Mos coffee shop is local coffee shop of toronto.",
+        creator: "Local owner",
+        imgURL: "https://s3-media0.fl.yelpcdn.com/bphoto/Npa5qWA2-KjoffJAs99XLg/348s.jpg",
+        type: "Local",
+        tags: ["coffee", "local"]
+      },{
+        id: 6,
+        title: "Good earth coffee house",
+        description: "Good earth coffee house is famous brand in calgary.",
+        creator: "Unknown",
+        imgURL: "https://lh3.googleusercontent.com/p/AF1QipOse5seR-K_OB_ev2Kgnd23sBNfP66BqnmAyB8=s1360-w1360-h1020-rw",
+        type: "Local",
+        tags: ["Famous", "calgary"]
       }
       
     ];
---
+++ M_Kaur_MyFavouriteCoffeeShops/src/app/typedecision.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TypedecisionPipe } from './typedecision.pipe';
+
+describe('TypedecisionPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TypedecisionPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
